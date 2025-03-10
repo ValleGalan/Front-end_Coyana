f976432ee0fea663b02ea4f0d15d39c2dd16b6d2
vallegalan <vallegalan810@gmail.com>
2025-03-10 15:11:17 -0300

    Cambios en el tamaño del banner

--- src/App.js
+++ src/App.js
@@ -59,7 +59,6 @@ function App() {
     fetchUserAddToCart()
 
   }, [])
-  // const isFundacionPuna = location.pathname.startsWith('/fundacion_puna');
 
   return (
     <div className='font-mono'>
@@ -71,11 +70,13 @@ function App() {
         fetchUserAddToCart
       }}>
         <ToastContainer position='top-center' />
+       {/*
         <Popup />
         <DiscountPopup />
-        {/* Barra de anuncio fija */}
-        <div className="fixed top-0 left-0 right-0 z-50 bg-anaranjado-claro-color">
-          <AnnouncementBar />
+       */}
+        {/* Barra fija de nros de contacto*/}
+        <div className="fixed top-[altura-exacta] left-0 z-50 w-full shadow-md">
+        <AnnouncementBar />
         </div>
 
         {/* Header fijo debajo de la barra de anuncio */}
@@ -85,7 +86,7 @@ function App() {
 
         {/* Contenido principal */}
 
-        <main className='min-h-[calc(100vh-120px)] pt-32'>
+        <main className='min-h-[calc(100vh-120px)] pt-15'>
           <Outlet />
         </main>
         <Footer />
@@ -96,54 +97,4 @@ function App() {
 
 export default App;
 
-/*
-
-import React from 'react';
-import Header from './components/FundacionPuna/Header'
-import Context from './context'; 
-import { Outlet } from 'react-router-dom'; //  Outlet para renderizar rutas hijas
-import Footer from './components/FundacionPuna/Footer'
-
-function App() {
-  return (
-    <div className="font-mono">
-      <Context.Provider value={{   }}>
-      <Header></Header>
-
-      <main className="ml-64 p-8">  }
-        <Outlet />  
-      </main>
-      <div className="ml-64">
-      <Footer></Footer>
-      </div>
-      </Context.Provider>
-    </div>
-  );
-}
-export default App;
-
  
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import CoyanaApp from './App_coyana'; // componente Coyana
-import Home from './pages/Blog/Home'
-import Mision from './pages/Blog/Mision'
-import Valores from './pages/Blog/Valores'
-import QuienesSomos from './pages/Blog/QuienesSomos'
-import Blog from './pages/Blog/Blog'
-
- <BrowserRouter>
-        
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/mision" element={<Mision />} />
-            <Route path="/valores" element={<Valores />} />
-            <Route path="/quienes_somos" element={<QuienesSomos />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/coyana" element={<CoyanaApp />} />
-          </Routes>
-
-        </BrowserRouter>
- 
-
-*/
